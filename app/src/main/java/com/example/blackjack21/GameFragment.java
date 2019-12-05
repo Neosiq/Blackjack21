0e6bd2df222466ad119b724d20c6220dc736c690
Brian Zou <neosiq@gmail.com>
2019-12-05 13:42:35 -0800

    commit#19 textview stuff

--- app/src/main/java/com/example/blackjack21/GameFragment.java
+++ app/src/main/java/com/example/blackjack21/GameFragment.java
@@ -1,7 +1,36 @@
 package com.example.blackjack21;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class GameFragment extends Fragment {
+
+    private TextView dealer;
+    private TextView player;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View rootView = inflater.inflate(R.layout.fragment_game, container, false);
+        wireWidgets(rootView);
+        return rootView;
+    }
+
+    private void wireWidgets (View rootView) {
+        dealer = rootView.findViewById(R.id.game_fragment_dealer_textview);
+        player = rootView.findViewById(R.id.game_fragment_player_textview);
+    }
+
 
 }
