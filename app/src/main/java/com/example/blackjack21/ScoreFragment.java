32a494f6ef06c6bfda0d1a8b4b84dc6ede3dcdad
Brian Zou <neosiq@gmail.com>
2019-12-05 13:23:39 -0800

    commit#16 scorefrag update

--- app/src/main/java/com/example/blackjack21/ScoreFragment.java
+++ app/src/main/java/com/example/blackjack21/ScoreFragment.java
@@ -1,8 +1,13 @@
 package com.example.blackjack21;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.blackjack21.R;
@@ -13,10 +18,18 @@ public class ScoreFragment extends Fragment {
 
 
 
-    private void WireWidgets (View rootView) {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
+        View rootView = inflater.inflate(R.layout.fragment_score, container, false);
+
+        wireWidgets(rootView);
+        return rootView;
+    }
+
+    private void wireWidgets (View rootView) {
         score = rootView.findViewById(R.id.game_fragment_textview);
-
     }
 }
