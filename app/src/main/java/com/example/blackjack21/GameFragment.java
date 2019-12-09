471470177ce934c478491392c23b64f23c42d5b5
Brian Zou <neosiq@gmail.com>
2019-12-09 12:59:28 -0800

    commit#21 everything initialized and wired

--- app/src/main/java/com/example/blackjack21/GameFragment.java
+++ app/src/main/java/com/example/blackjack21/GameFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,8 +14,23 @@ import androidx.fragment.app.Fragment;
 
 public class GameFragment extends Fragment {
 
+    //instance variables: dealer & player textview
     private TextView dealer;
     private TextView player;
+    //dealer's cards
+    private ImageView dcard1;
+    private ImageView dcard2;
+    private ImageView dcard3;
+    private ImageView dcard4;
+    // player's cards
+    private ImageView pcard1;
+    private ImageView pcard2;
+    private ImageView pcard3;
+    private ImageView pcard4;
+    // buttons: hit and stand
+    private Button hit;
+    private Button stand;
+
 
 
 
@@ -27,10 +44,23 @@ public class GameFragment extends Fragment {
         return rootView;
     }
 
+
     private void wireWidgets (View rootView) {
         dealer = rootView.findViewById(R.id.game_fragment_dealer_textview);
         player = rootView.findViewById(R.id.game_fragment_player_textview);
+        dcard1 = rootView.findViewById(R.id.game_fragment_dealer_card1);
+        dcard2 = rootView.findViewById(R.id.game_fragment_dealer_card2);
+        dcard3 = rootView.findViewById(R.id.game_fragment_dealer_card3);
+        dcard4 = rootView.findViewById(R.id.game_fragment_dealer_card4);
+        pcard1 = rootView.findViewById(R.id.game_fragment_player_card1);
+        pcard2 = rootView.findViewById(R.id.game_fragment_player_card2);
+        pcard3 = rootView.findViewById(R.id.game_fragment_player_card3);
+        pcard4 = rootView.findViewById(R.id.game_fragment_player_card4);
+        hit = rootView.findViewById(R.id.game_fragment_hit);
+        stand = rootView.findViewById(R.id.game_fragment_stand);
     }
+
+
 
 
 }
